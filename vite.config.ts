b99429094b5b176026a289f67a0585cff1c36906
Clément Le Goffic <clementlegoffic@gmail.com>
2026-06-28 17:55:04 +0200

    Set base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/web-immersive_rp/',
+  base: '/site-immersive/',
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1200,
